54e9283fb2f5aafe6b91b02be37deb376bc5490e
dalakoti07 <saurabhdalakoti07@gmail.com>
2024-07-21 14:35:42 +0530

    Added Image Picker

---
+++ README.md
@@ -0,0 +1,10 @@
+# Meme generator
+- Functional Requirements
+  - pick meme template from your device
+  - put emojis
+
+- Non functional
+  - State of art android practises
+  - Use SDP for compose
+
+
--- app/src/main/java/com/dalakoti07/android/memegenerator/MainActivity.kt
+++ app/src/main/java/com/dalakoti07/android/memegenerator/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.dalakoti07.android.memegenerator
 
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.dalakoti07.android.memegenerator.ui.theme.MemeGeneratorTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +35,96 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MemeGeneratorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Meme generator App")
-                }
+                HomeScreen()
             }
         }
     }
 }
 
+/**
+ * Are these image picker APIs backward compatible??
+ */
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ImagePicker() {
+    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
+    val context = LocalContext.current
+    var permissionGranted by remember { mutableStateOf(false) }
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            permissionGranted = isGranted
+        }
     )
+
+    LaunchedEffect(key1 = true) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionGranted = true
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+        }
+    }
+
+    val imagePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri ->
+        uri?.let {
+            context.contentResolver.openInputStream(it)?.use { inputStream ->
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                imageBitmap = bitmap?.asImageBitmap()
+            }
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                12.dp,
+            ),
+    ) {
+        imageBitmap?.let { img ->
+            Image(
+                bitmap = img,
+                contentDescription = "Selected Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+        Button(
+            modifier = Modifier.padding(
+                top = 10.dp,
+            ),
+            onClick = {
+                if (permissionGranted) {
+                    imagePickerLauncher.launch("image/*")
+                } else {
+                    requestPermissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            },
+        ) {
+            Text(
+                if (imageBitmap == null)
+                    "Pick an Image"
+                else "Change Image"
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    ImagePicker()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     MemeGeneratorTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
