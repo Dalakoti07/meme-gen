181b46e766d80b304743a0630a77d267f0176422
dalakoti07 <saurabhdalakoti07@gmail.com>
2025-05-03 18:40:30 +0530

    bumped min sdk to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dalakoti07.android.memegenerator"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
